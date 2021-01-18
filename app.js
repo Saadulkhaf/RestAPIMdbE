bcfff483c15e509be3033084cc6e148a32894e35
saadulkhaf <saadafzal995@gmail.com>
2021-01-19 04:04:47 +0500

    api completed

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const postsRoute = require("./routes/posts");
+const bodyParser = require("body-parser");
 
-// Routes
+app.use(bodyParser.json());
+app.use("/posts", postsRoute);
+
 app.get("/", (req, res) => {
-  res.send("At home page");
+  res.send("At Home");
 });
-
 // connecting to database
 
 mongoose.connect(
-  "mongodb://127.0.0.1:27017",
+  "mongodb://127.0.0.1:27017/restdb",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("Connected to db");
