9d8d1ba96463065deae70bcc127dc7480aaaa555
saadulkhaf <saadafzal995@gmail.com>
2021-01-19 01:41:16 +0500

    db connected with mongoose

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
@@ -5,5 +6,15 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("At home page");
 });
+
+// connecting to database
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to db");
+  }
+);
 
 app.listen("3000");
